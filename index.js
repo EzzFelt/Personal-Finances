baaeb43d9f597a1ad83f0d488e6de7f732b243c4
Enzo Feltrin <enzobrexo@gmail.com>
2025-02-10 00:52:13 -0300

    Creating the saveTransactions, this function saves the user transaction in backend

--- index.js
+++ index.js
@@ -75,3 +75,30 @@ function updateBalance() {
   }
   
   document.addEventListener('DOMContentLoaded', setup)
+
+// This function saves the user transaction in the backend, using the method POST
+  async function saveTransaction(ev) {
+    ev.preventDefault()
+  
+    const name = document.querySelector('#name').value
+    const amount = parseFloat(document.querySelector('#amount').value)
+  
+    const response = await fetch('http://localhost:3000/transactions', {
+      method: 'POST',
+      body: JSON.stringify({ name, amount }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const transaction = await response.json()
+    transactions.push(transaction)
+    renderTransaction(transaction)
+  
+    ev.target.reset()
+    updateBalance()
+  }
+  
+  // ...
+  
+  document.addEventListener('DOMContentLoaded', setup)
+  document.querySelector('form').addEventListener('submit', saveTransaction)
