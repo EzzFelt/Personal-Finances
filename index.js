4449a1fc7a4780076c1873e10c9330a71a43e855
Enzo Feltrin <enzobrexo@gmail.com>
2025-02-10 00:48:13 -0300

    Created functions for Transaction amount, render transactions, fetch transactions and update the user balance

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let transactions = []
 
 // Creating the container for each transaction by ID
 function createTransactionContainer(id) {
@@ -13,4 +14,54 @@ function createTransactionContainer(id) {
     title.classList.add('transaction-title')
     title.textContent = name
     return title
+  }
+
+// Using the number formatter API for the user Currency which is BRL
+  function createTransactionAmount(amount) {
+    const span = document.createElement('span')
+    span.classList.add('transaction-amount')
+    const formater = Intl.NumberFormat('pt-BR', {
+      compactDisplay: 'long',
+      currency: 'BRL',
+      style: 'currency',
+    })
+    const formatedAmount = formater.format(amount)
+
+// If the value is positive it'll be credit, otherwise will be debit
+    if (amount > 0) {
+      span.textContent = `${formatedAmount} C`
+      span.classList.add('credit')
+    } else {
+      span.textContent = `${formatedAmount} D`
+      span.classList.add('debit')
+    }
+    return span
+  }
+
+// Function that renders the user's transaction in the screen
+  function renderTransaction(transaction) {
+    const container = createTransactionContainer(transaction.id)
+    const title = createTransactionTitle(transaction.name)
+    const amount = createTransactionAmount(transaction.amount)
+  
+    document.querySelector('#transactions').append(container)
+    container.append(title, amount)
+  }
+
+// Getting the data from the backend
+  async function fetchTransactions() {
+    return await fetch('http://localhost:3000/transactions').then(res => res.json())
+  }
+
+
+// This function updates the user balance by getting the information from array transactions
+function updateBalance() {
+    const balanceSpan = document.querySelector('#balance')
+    const balance = transactions.reduce((sum, transaction) => sum + transaction.amount, 0)
+    const formater = Intl.NumberFormat('pt-BR', {
+      compactDisplay: 'long',
+      currency: 'BRL',
+      style: 'currency'
+    })
+    balanceSpan.textContent = formater.format(balance)
   }
