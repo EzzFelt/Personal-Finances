54973eb297a11866b951fdb6d76cc8f0d0001885
Enzo Feltrin <enzobrexo@gmail.com>
2025-02-10 00:56:18 -0300

    Updating the function saveTransaction

--- index.js
+++ index.js
@@ -78,9 +78,24 @@ function updateBalance() {
   async function saveTransaction(ev) {
     ev.preventDefault()
   
+    const id = document.querySelector('#id').value
     const name = document.querySelector('#name').value
     const amount = parseFloat(document.querySelector('#amount').value)
   
+    if (id) {
+        const response = await fetch(`http://localhost:3000/transactions/${id}`, {
+          method: 'PUT',
+          body: JSON.stringify({ name, amount }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        const transaction = await response.json()
+        const indexToRemove = transactions.findIndex((t) => t.id === id)
+        transactions.splice(indexToRemove, 1, transaction)
+        document.querySelector(`#transaction-${id}`).remove()
+        renderTransaction(transaction)
+      } else {
     const response = await fetch('http://localhost:3000/transactions', {
       method: 'POST',
       body: JSON.stringify({ name, amount }),
@@ -95,6 +110,7 @@ function updateBalance() {
     ev.target.reset()
     updateBalance()
   }
+}
 
 // This function creates an edit button for each transaction
   function createEditTransactionBtn(transaction) {
